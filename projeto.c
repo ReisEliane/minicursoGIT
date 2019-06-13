eb21bba26f99717a316d0e13ce1508eadf90f69d
Eliane Reis <elianereisbi@gmail.com>
2019-06-13 20:03:15 -0300

    Adicionando multiplicação

--- projeto.c
+++ projeto.c
@@ -5,14 +5,23 @@ int soma(int a, int b)
 	int soma_num = a + b;
 	return soma_num;
 }
+
 int subtracao(int a, int b)
 {
 	int sub = a - b;
 	return sub;
 }
+
+int multiplicacao(int a, int b)
+{
+	int mult = a * b;
+	return mult;
+}
+
 int main()
 {
 	printf ("A soma dos dois numeros e: %d\n",soma(5,5));
 	printf ("A subtracao dos dois numeros e: %d\n",subtracao(5,2));
+	printf("A multiplicacao dos numeros e: %d\n",multiplicacao(10,5));
 	return 0;
 }
